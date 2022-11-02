9af241aeb18f1e685be92abf9a3861dc8b8ed2ed
andrea scacchi <andrea.scacchi@live.com>
2022-11-02 16:54:22 +0100

    Final Commit, TODO bonus

--- Program.cs
+++ Program.cs
@@ -19,7 +19,80 @@ Console.WriteLine("Hello, World!");
 
 Product product1 = new Product();
 
+//create new product
+product1.SetName("TestName");
+product1.SetDescription("Test Description Lorem Ipsum");
+product1.SetPrice(123.45);
+product1.SetIva(12);
+
+//print to console
+Console.WriteLine(product1.GetProdKey);
+Console.WriteLine(product1.GetName());
+Console.WriteLine(product1.GetDescription());
+Console.WriteLine(product1.GetPrice());
+Console.WriteLine(product1.GetIva());
+
+
+
 public class Product
 {
+    //class attributes
+    private int prodKey;
+    private string name;
+    private string description;
+    private double price;
+    private int iva;
 
+    //constructor
+    public Product()
+    {
+        Random random = new Random();
+        prodKey = random.Next(1, 123456);
+    }
+
+    //methods
+    public int GetProdKey()
+    {
+        return prodKey;
+    }
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    public string GetName()
+    {
+        return name;
+    }
+    public void SetDescription(string description)
+    {
+        this.description = description;
+    }
+    public string GetDescription()
+    {
+        return description;
+    }
+    public void SetPrice(double price)
+    {
+        this.price = price;
+    }
+    public double GetPrice()
+    {
+        return price;
+    }
+    public void SetIva(int iva)
+    {
+        this.iva = iva;
+    }
+    public int GetIva()
+    {
+        return iva;
+    }
+    public double GetPricePostTax()
+    {
+        return price + (price / 100 * iva);
+    }
+    public string GetFullName()
+    {
+        return prodKey + " " + name;
+    }
 }
